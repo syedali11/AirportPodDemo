06a0157ff02816158bae58472aeaeba50d451ef7
Syed Ali <syed.ali@nagarro.com>
2020-10-16 02:29:43 +0530

    commit from Xcode to pod

---
+++ AirportPodDemo/Classes/Airport.swift
@@ -0,0 +1,16 @@
+//
+//  AirportUtility.swift
+//  AirportPod
+//
+//  Created by Syed Ali on 15/10/20.
+//
+
+import Foundation
+import CoreData
+
+public typealias FetchItemsCompletionBlock = (_ success: Any?, _ error: NSError?) -> Void
+
+public struct Constants {
+    public static let CellReuseId = "cell"
+}
+
