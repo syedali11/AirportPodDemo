83e210439de7c0645e5350c73550376dc6d19380
Syed Ali <syed.ali@nagarro.com>
2020-10-16 18:28:18 +0530

    comment added in Airport pod

--- AirportPodDemo/Classes/Airport.swift
+++ AirportPodDemo/Classes/Airport.swift
@@ -10,6 +10,8 @@ import CoreData
 
 public typealias FetchItemsCompletionBlock = (_ success: Any?, _ error: NSError?) -> Void
 
+//constansts used in Airport App
+
 public struct Constants {
     public static let CellReuseId = "cell"
 }
